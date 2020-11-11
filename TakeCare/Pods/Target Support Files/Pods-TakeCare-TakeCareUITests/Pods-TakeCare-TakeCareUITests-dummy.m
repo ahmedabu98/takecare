cbb36cfc7161df7ee22ac874d7a5c0191b4b8da6
Ahmed <a.abualsaud98@gmail.com>
2020-11-10 21:09:07 -0500

    project init

---
+++ TakeCare/Pods/Target Support Files/Pods-TakeCare-TakeCareUITests/Pods-TakeCare-TakeCareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TakeCare_TakeCareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TakeCare_TakeCareUITests
+@end
---
+++ TakeCare/TakeCare/loginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  loginViewController.swift
+//  TakeCare
+//
+//  Created by Ahmed Abualsaud on 11/10/20.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class loginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
