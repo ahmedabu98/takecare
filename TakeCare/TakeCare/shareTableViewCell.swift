0c27df2e9d80f68ee14c22a6fb20cfcb370857ad
Ramon Novo Aragon <ramonnovo@MacBook-Pro-de-Ramon.local>
2020-11-18 18:32:19 -0500

    Added view controllers for the past logs, need to determine how we are populating those. Also added table view controller to share with provider, but cell size does not change

---
+++ TakeCare/TakeCare/shareTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  shareTableViewCell.swift
+//  TakeCare
+//
+//  Created by Ramon Novo Aragon on 11/18/20.
+//
+
+import UIKit
+
+class shareTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
